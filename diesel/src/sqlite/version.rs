6843a0309e56fb391f520a95b85097529b5cb28e
Daniel Sousa <da.arada@gmail.com>
2023-09-24 00:09:52 +0000

    Changes to be committed: 	new file:   diesel/src/sqlite/version.rs

---
+++ diesel/src/sqlite/version.rs
@@ -0,0 +1,42 @@
+/// Returns the SQLite version as an integer; e.g., `3016002` for version
+/// 3.16.2.
+///
+/// See [`sqlite3_libversion_number()`](https://www.sqlite.org/c3ref/libversion.html).
+#[inline]
+#[must_use]
+pub fn get_sqlite_version_number() -> i32 {
+    let query = "SELECT sqlite_version_number()";
+    let result = sql::<sql_types::Text>(query).load::<String>(conn).unwrap();
+    result[0]
+}
+
+/// Returns the SQLite version as a string; e.g., `"3.16.2"` for version 3.16.2.
+///
+/// See [`sqlite3_libversion()`](https://www.sqlite.org/c3ref/libversion.html).
+#[inline]
+#[must_use]
+pub fn get_sqlite_version(conn: &mut SqliteConnection) -> &str {
+    let query = "SELECT sqlite_version()";
+    let result = sql::<sql_types::Text>(query).load::<String>(conn).unwrap();
+    result[0]
+}
+
+#[test]
+fn get_sqlite_version() {
+    let mut connection = SqliteConnection::establish(":memory:").unwrap();
+
+    let sqlite_version = get_sqlite_version(&mut connection);
+
+    assert!(sqlite_version.split(".").collect::<Vec<_>>().len(), 3);
+}
+
+#[test]
+fn get_sqlite_version_number() {
+    std::ops::contains;
+
+    let mut connection = SqliteConnection::establish(":memory:").unwrap();
+
+    let sqlite_version_number = get_sqlite_version_number(&mut connection);
+
+    assert!((3000000..4000000).contains(&sqlite_version_number), true);
+}
